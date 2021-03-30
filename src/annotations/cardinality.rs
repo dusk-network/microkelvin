967985fef00adcc8c64f90516f0656e2e2c4a00f
Kristoffer Ström <kristoffer@rymdkoloni.se>
2021-03-30 17:58:55 +0200

    Add proper license headers

--- src/annotations/cardinality.rs
+++ src/annotations/cardinality.rs
@@ -1,3 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
 /// Annotation to keep track of the cardinality,
 /// i.e. the amount of elements in a collection
 use crate::annotations::{Ann, Annotation};
